7c41b19f75e3a450f2e3064748073b8ccb65721f
s-miki-07 <SAKAIRI.Miki@nims.co.jp>
2015-09-17 16:42:48 +0900

    3_11_complete

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :request do
-  describe "GET /static_pages" do
-    it "works! (StaticPages)" do
-      get static_pages_index_path
-      expect(response).to have_http_status(200)
+RSpec.describe "Static_pages", type: :request do
+  describe "GET /Static_pages" do
+    it "works! (static_pages)" do
+     #get Static_pages
+      visit '/static_pages/home'
+      #expect(response).to have_http_status(200)
+       expect(page).to have_content("Sample App")
     end
   end
 end
