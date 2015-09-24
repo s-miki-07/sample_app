326e97e7c4bcfe836b83c535ed07749a4cfbbe78
s-miki-07 <SAKAIRI.Miki@nims.co.jp>
2015-09-24 14:13:25 +0900

    3.19

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe "Static_pages", type: :request do
       #expect(response).to have_http_status(200)
        expect(page).to have_content("Sample App")
     end
+
+   it "shoule have the title 'home'" do
+       visit '/static_pages/home'
+       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")  
+    end
   end
 
 
@@ -17,6 +22,11 @@ describe "Help page" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+  it "shoule have the title 'home'" do
+       visit '/static_pages/home'
+       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")  
+    end
   end
 
   describe "About page" do
@@ -24,6 +34,11 @@ describe "Help page" do
     visit '/static_pages_/about'
     expect(page).to have_content('About us')
   end
+
+  it "shoule have the title 'home'" do
+       visit '/static_pages/home'
+       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")  
+    end
  end
 end
 
