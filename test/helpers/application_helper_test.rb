3dcdb31151522eba83e3477fba4aaa65ee301cce
s-miki-07 <SAKAIRI.Miki@nims.co.jp>
2015-12-03 11:55:48 +0900

    6.2.2

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,9 +2,13 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-   assert_equal full_title,         FILL_IN
-   assert_equal full_title("Help"), FILL_IN
-  end
-end
+    
+    a0 = "Home | Ruby on Rails Tutorial Sample App" 
+    assert_equal full_title("Home"), a0, "test1"
 
+    a1 = "Help | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Help"), a1, "test2"
+  end
+
+end
 
