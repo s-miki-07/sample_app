9ef4979905b6fb48d8c8f0bb16443c54b5beffb7
s-miki-07 <SAKAIRI.Miki@nims.co.jp>
2015-12-07 16:46:15 +0900

    6_end

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -41,7 +41,7 @@ class UserTest < ActiveSupport::TestCase
   end
 	
   test "email validation should reject valid addresses" do
-	invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com]
+	invalid_addresses = %w[foo@bar..com,user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com]
 	invalid_addresses.each do |invalid_address|
 	 @user.email = invalid_address
    assert_not@user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -56,13 +56,22 @@ class UserTest < ActiveSupport::TestCase
 
   end	
 
-  test "password should be present (nonblank)" do
-      @user.password = @user.password_confirmation = " " * 6
-      assert_not @user.valid?
+ #  test "password should be present (nonblank)" do
+ #     @user.password = @user.password_confirmation = " " * 6
+ #     assert_not @user.valid?
+ # end
+
+  test "email addresses should be saves as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
   test "password should have a minimum length" do
       @user.password = @user.password_confirmation = "a" * 5 
       assert_not @user.valid?
   end
+    
+
 end
