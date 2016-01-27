552cb72156518b84359f3e54735b919c134d80dc
s-miki-07 <SAKAIRI.Miki@nims.co.jp>
2016-01-27 14:18:05 +0900

    7.3.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_paramas)
+    @user = User.new(user_params)
     if @user.save
 
       #Handle a succellful save.
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
     else
 
       render 'new'
+    end
 
  end
 
@@ -25,7 +26,7 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
 
-      end
+
 
   end
 end
